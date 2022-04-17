4a101305039b49c84d6de2ee8779a50378b88110
Frank Schmitt <frankschmitt@gmx.de>
2022-04-17 12:32:48 +0200

    rusty roguelike: basic setup

---
+++ rusty-roguelike/src/map.rs
@@ -0,0 +1,27 @@
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+// Map, consisting of NUM_TILES tiles
+pub struct Map {
+    pub tiles: Vec<TileType>
+}
+
+// convert (x,y) coordinates into a map index
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+}
